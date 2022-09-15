7982144a8ad0ce31d24f09a4e89a2553c01e9bdb
Ayush Kumar <ayush.kumar@netstairs.com>
2022-09-15 22:39:29 +0530

    homepage UI

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,163 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="main">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          v-show="isMobile"
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title class="q-pa-md row item-center">
+          <img src="../assets/Logo.png" />
+        </q-toolbar-title>
+
+        <q-btn flat round dense class="q-ml-md md" label="Home" />
+        <q-btn flat round dense class="q-ml-md md" label="AboutUs" />
+        <q-btn flat round dense class="q-ml-md md" label="Testinomial" />
+        <q-btn flat round dense class="q-ml-md md" label="ContactUs" />
+        <q-btn
+          flat
+          dense
+          round
+          class="q-ml-md md"
+          :icon="matAccountCircle"
+          aria-label="Menu"
+        >
+          <q-menu>
+            <q-list style="min-width: 100px,max-height:50px">
+              <!-- <q-item clickable v-close-popup>
+                <q-item-section>Login</q-item-section>
+              </q-item> -->
+              <Login/>
+
+              <q-item clickable v-close-popup>
+                <q-item-section>Signup</q-item-section>
+              </q-item>
+            </q-list></q-menu
+          ></q-btn
+        >
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      v-if="isMobile"
+      :breakpoint="500"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page>
+      <router-view :isMobile="isMobile" />
+    </q-page>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref, computed } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+import { matAccountCircle } from "@quasar/extras/material-icons";
+import Login from "src/pages/Login.vue";
+
+const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+export default defineComponent({
+  created() {
+    this.matAccountCircle = matAccountCircle;
+  },
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+    Login
+},
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const isMobile = ref(false);
+
+    window.onresize = () => {
+      if (window.innerWidth <= 760) {
+        isMobile.value = true;
+      } else {
+        isMobile.value = false;
+      }
+    };
+    window.onload = () => {
+      if (window.innerWidth <= 760) {
+        isMobile.value = true;
+      } else {
+        isMobile.value = false;
+      }
+    };
+    const toggleLeftDrawer = () => {
+      leftDrawerOpen.value = !leftDrawerOpen.value;
+      console.log("call");
+    };
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      isMobile,
+      toggleLeftDrawer,
+    };
+  },
+});
+</script>
---
+++ src/pages/Login.vue
@@ -0,0 +1,162 @@
+
+
+<template>
+     <q-btn label="Close Icon" color="primary" @click="icon = true" />
+     <q-dialog full-width style="width: 300px" v-model="icon">
+      <q-card>
+        <q-card-section class="row items-center q-pb-none">
+        
+          <q-space />
+          <!-- <q-btn icon="close" flat round dense v-close-popup /> -->
+        </q-card-section>
+        <q-card-section>
+            <div class="row">
+        <div v-bind:class="{'justify-center': $q.screen.md || $q.screen.sm ||$q.screen.xs}"
+            class="col-12 col-md-6 flex content-center borderCheck">
+            <q-card style="width:100%" >
+
+                <q-card-section>
+                    <div class="q-pt-lg">
+                        <div class="col text-h6 ellipsis flex justify-center">
+                            <h2 class="text-h4 text-uppercase q-my-none align-left">Travel Rover</h2>
+                        </div>
+                    </div>
+                </q-card-section>
+                <q-card-section>
+                    <q-form class="q-gutter-md" @submit.prevent="submitForm">
+                        <q-input label="Email" v-model="login.username">
+                        </q-input>
+                        <q-input label="Password" type="password" v-model="login.password">
+                        </q-input>
+                        <div>
+                            <q-btn class="full-width" color="primary" label="Login" type="submit" rounded></q-btn>
+
+                          
+                        </div>
+                        <q-text class="orSection">Or</q-text>
+                        <div>
+                            <q-btn class="full-width" color="primary" label="Login with OTP" type="submit" rounded>
+                            </q-btn>
+                        </div>
+                        <q-text>Forget Password <a href="
+                            #">Click Here</a></q-text>
+                    </q-form>
+
+                </q-card-section>
+            </q-card>
+        </div>
+        <div v-bind:class="{'justify-center': $q.screen.md || $q.screen.sm ||$q.screen.xs}"
+            class=" borderCheck col-12 col-md-6 flex content-center">
+            <q-card style="imageView" v-bind:style="$q.screen.lt.sm ? {'width': '100%'} : {'width': '100%'}">
+            
+                <div class="q-pa-md" style="width:80%; margin:0px auto">
+                    <q-carousel swipeable animated v-model="slide" :autoplay="autoplay" ref="carousel" infinite>
+                        <q-carousel-slide :name="1" img-src="https://cdn.quasar.dev/img/mountains.jpg" />
+                        <q-carousel-slide :name="2" img-src="https://cdn.quasar.dev/img/parallax1.jpg" />
+                        <q-carousel-slide :name="3" img-src="https://cdn.quasar.dev/img/parallax2.jpg" />
+                        <q-carousel-slide :name="4" img-src="https://cdn.quasar.dev/img/quasar.jpg" />
+
+                        <template v-slot:control>
+                            <q-carousel-control position="top-right" :offset="[18, 18]"
+                                class="text-white rounded-borders"
+                                style="background: rgba(0, 0, 0, .3); padding: 4px 8px;">
+                                <q-btn icon="close" flat round dense v-close-popup />
+                            </q-carousel-control>
+
+                            <q-carousel-control position="bottom-right" :offset="[18, 18]" class="q-gutter-xs">
+                                <q-btn push round dense color="orange" text-color="black" icon="arrow_left"
+                                    @click="$refs.carousel.previous()" />
+                                <q-btn push round dense color="orange" text-color="black" icon="arrow_right"
+                                    @click="$refs.carousel.next()" />
+                            </q-carousel-control>
+                        </template>
+                    </q-carousel>
+                </div>
+            </q-card>
+        </div>
+
+
+    </div>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+</template>
+<script>
+    import { useQuasar } from 'quasar'
+    import { mapActions } from 'vuex'
+    import { ref } from 'vue'
+    
+    let $q
+    export default {
+        
+      name: 'LoginLayout',
+      setup () {
+    return {
+      slide: ref(1),
+      autoplay: ref(true),
+      icon: ref(false),
+    }
+  },
+      data () {
+        return {
+          login: {
+            username: 'Joabson',
+            password: 'a2d4g6j8'
+          }
+        }
+      },
+      methods: {
+        ...mapActions('auth', ['doLogin']),
+        async submitForm () {
+          if (!this.login.username || !this.login.password) {
+            $q.notify({
+              type: 'negative',
+              message: 'Os dados informados são inválidos.'
+            })
+          } else if (this.login.password.length < 6) {
+            $q.notify({
+              type: 'negative',
+              message: 'A senha deve ter 6 ou mais caracteres.'
+            })
+          } else {
+            try {
+              await this.doLogin(this.login)
+              const toPath = this.$route.query.to || '/admin'
+              this.$router.push(toPath)
+            } catch (err) {
+              if (err.response.data.detail) {
+                $q.notify({
+                  type: 'negative',
+                  message: err.response.data.detail
+                })
+              }
+            }
+          }
+        }
+      },
+      mounted () {
+        $q = useQuasar()
+      }
+    }
+    </script>
+    
+    <style scoped>
+    .wave {
+        position: fixed;
+        height: 100%;
+        left: 0;
+        bottom: 0;
+        z-index: -1;
+    }
+    
+
+    
+    .orSection {
+        display: flex;
+        justify-content: center;
+    }
+    .q-card__section--vert{
+  padding: 5px !important;
+    }
+   
+    </style>
