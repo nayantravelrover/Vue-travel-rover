edadbb62f78f43b2ebab9aad11a1052ce01679d2
Tanay Desai <tanay.desai@sutradhar.co.in>
2022-10-05 00:46:18 +0530

    Alignments Fixed

---
+++ src/pages/CardContainer.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="gt-xs">
+        <div class="container40">
+            <text class="text50">Goa</text>
+            <text class="text51">God's Own Country</text>
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
+<style>
+.text51{
+        width: 385px;
+            height: 60px;
+        
+            font-family: 'Poppins';
+            font-style: normal;
+            font-weight: 500;
+            font-size: 40px;
+            line-height: 60px;
+            /* identical to box height */
+        
+            /* White/White 100 */
+            color: #FFFFFF;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 1;
+            flex-grow: 0;
+    }
+.text50{
+        width: 150px;
+            height: 108px;
+        
+            font-family: 'Poppins';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 72px;
+            line-height: 108px;
+            /* identical to box height */
+        
+            /* White/White 100 */
+            color: #FFFFFF;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
+            margin: -10px 0px;
+    }
+.container40{
+        display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 32px 63px;
+        
+            position: absolute;
+            width: 511px;
+            height: 222px;
+            left: 465px;
+            top: 300px;
+        
+            background: rgba(0, 0, 0, 0.2);
+            border-radius: 10px;
+    }
+</style>
---
+++ src/pages/MobileCard.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="lt-md">
+        <div class="container60">
+            <text class="text60">Goa</text>
+            <text class="text61">God's Own Country</text>
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
+<style>
+.text61{
+        width: 385px;
+            height: 30px;
+        
+            font-family: 'Poppins';
+            font-style: normal;
+            font-weight: 600;
+            font-size: 25px;
+            line-height: 20px;
+            margin-top: 50px;
+            margin-left: 140px;
+            /* identical to box height */
+        
+            /* White/White 100 */
+            color: #FFFFFF;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 1;
+            flex-grow: 0;
+    }
+.text60{
+        width: 100px;
+            height: 28px;
+        
+            font-family: 'Poppins';
+            font-style: normal;
+            font-weight: 600;
+            font-size: 40px;
+            line-height: 48px;
+            
+            /* identical to box height */
+        
+            /* White/White 100 */
+            color: #FFFFFF;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
+            margin: -10px 0px;
+    }
+.container60 {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 32px 63px;
+    
+            position: absolute;
+            width: 311px;
+            height: 150px;
+            left: 55px;
+            top: 179px;
+    
+            background: rgba(0, 0, 0, 0.2);
+            border-radius: 10px;
+        }
+</style>
