7f503e424a9893ac467580a5a9472c954c34974d
Anuj Vadecha <anujvadecha@anujs-mbp.mynetworksettings.com>
2022-10-09 17:31:02 -0400

    beautification pgase 1

---
+++ src/components/DayPreview.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row q-pa-md">
+    <div class="col-8">
+          <div class="text-bold q-pa-md">Day {{day_index+1}}</div>
+          <div class=" q-pa-md" v-html="day.description"></div>
+    </div>
+    <div class="col-4 q-pa-md">
+          <q-img :src="day.images[0]" class="q-ma-md"></q-img>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DayPreview",
+  props:["day","day_index"]
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/ItinaryBuilder.vue
+++ src/components/ItinaryBuilder.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="row">
-  <q-scroll-area class="col-6 q-pa-md" style="height: 100vh;">
-  <div >
-    <div class="text-bold q-ma-sm row" style="font-size: large">Place Name</div>
-    <q-input class="q-ma-sm row" outlined v-model="place_name" label="Place Name"/>
-    <div class="text-bold q-ma-sm row" style="font-size: large">Place Description</div>
-    <!-- <q-input class="q-ma-sm row" outlined v-model="place_description" label="Place Description"/> -->
+  <q-scroll-area   class="col-6 q-pa-md" style="height: 100vh;">
+  <div  class="col q-pa-md">
+<!--    <div class="q-ma-sm">-->
+    <div class="text-bold  row sectionheading" >Place Name</div>
+    <q-select outlined v-model="place_name" :options="[
+        'Ladakh', 'Himalayas'
+      ]"  />
+<!--    </div>-->
+    <div class="text-bold row sectionheading" >Place Description</div>
+    <q-input class="row" outlined v-model="place_description" />
 
-    <PicturedWYISG property_key="place_description" heading="Place Description"></PicturedWYISG>
-    <div class="q-pa-sm row">
+<!--    <PicturedWYISG property_key="place_description" heading="Place Description"></PicturedWYISG>-->
+    <div class="q-pt-sm">
       <q-uploader
-        color="grey"
+        bordered
+        color="white"
+        text-color="grey"
         url="http://admin.travelrover.in/travel-rover/api-files/"
         style=""
         auto-upload
@@ -31,12 +37,12 @@
 <!--          </q-card-section>-->
 <!--      </q-card>-->
     </div>
-    <div class="text-bold q-ma-sm row" style="font-size: large">Itinerary Name</div>
-    <q-input class="q-ma-sm row" outlined v-model="itinerary_name" label="Itinerary Name"/>
-    <div class="q-ma-sm row" style="font-size: large">
+    <div class="text-bold sectionheading row" >Itinerary Name</div>
+    <q-input class=" row" outlined v-model="itinerary_name" />
+    <div class="row">
       <div class="col-6">
-        <div class="q-ma-sm text-bold row" style="font-size: large">Start Date</div>
-        <q-input class="q-mr-md row" filled v-model="start_date" mask="date" :rules="['date']">
+        <div class=" text-bold row sectionheading" >Start Date</div>
+        <q-input class="row" filled v-model="start_date" mask="date" :rules="['date']">
           <template v-slot:append>
             <q-icon name="event" class="cursor-pointer">
               <q-popup-proxy cover transition-show="scale" transition-hide="scale">
@@ -51,11 +57,13 @@
         </q-input>
       </div>
       <div class="col-6">
-        <div class="q-ma-sm text-bold row" style="font-size: large">Number of Days</div>
-        <q-input class="row" outlined v-model="text" label="Itinerary Name"/>
+        <div class=" text-bold row sectionheading">Number of Days</div>
+        <q-input class="row" outlined v-model="text" />
       </div>
-      <div class="row text-bold q-ma-sm" style="font-size: large">Itinerary</div>
-      <div class="row" v-for="(day, index) in days" v-bind:key="index">
+      </div>
+      <div class="sectionheading" style="font-size: large">Itinerary</div>
+      <br>
+      <div class="" v-for="(day, index) in days" v-bind:key="index">
         <DayEditor :day_index="index" :day_content="day.description" :day_images="day.images"></DayEditor>
       </div>
       <q-btn  class="row" @click="days_add()">Add day</q-btn>
@@ -70,17 +78,18 @@
      <PicturedWYISG property_key="things_to_carry" heading="Things to Carry"></PicturedWYISG>
      <PicturedWYISG property_key="tour_rates" heading="Tour Rates"></PicturedWYISG>
 
-     <q-btn  class="row" @click="save_itinerary()">Add day</q-btn>
-
-
 <!--    </div>-->
-  </div>
+
     </div>
    </q-scroll-area>
-    <div class="col-6 q-pa-md bg-grey">
+    <div class="col-6 q-pa-md" style="background-color: #4B5563">
+      <div style="background-color: transparent">
+      <q-btn  text-color="white" icon="print" @click="print()"></q-btn>
+      <q-btn text-color="white"  class="" @click="save_itinerary()">Save Itinerary</q-btn>
+      </div>
       <q-scroll-area style="height: 100vh;">
     <ItineraryPreview ></ItineraryPreview>
-        </q-scroll-area>
+    </q-scroll-area>
     </div>
   </div>
 </template>
@@ -110,10 +119,7 @@ export default {
         this.$store.commit('day_add', 1)
         console.log(this.$store.state.itinerary_preview.days)
       },
-      save_itinerary: function(){
-        console.log(this.$store.state.itinerary_preview)
-        save_itinerary_api(this.$store.state.itinerary_preview)
-      },      
+
     onSelectFile() {
       const input = this.$refs.fileInput;
       const files = input.files;
@@ -203,5 +209,12 @@ export default {
 </script>
 
 <style scoped>
-
+  .sectionheading{
+    color: #4B5563;
+    font-size: large;
+    font-weight: bold;
+  }
+  div{
+    font-family: "Helvetica Neue";
+  }
 </style>
