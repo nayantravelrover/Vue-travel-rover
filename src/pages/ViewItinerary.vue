de4986221cc949f10a931f96693473f64a6ae2c4
Tanay Desai <tanay.desai@sutradhar.co.in>
2022-10-23 18:56:09 +0530

    Updated view itinerary

--- src/pages/ViewItinerary.vue
+++ src/pages/ViewItinerary.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="q-pa">
-        <q-card class="box1 gt-sm">
+        <div class="box1 gt-sm">
             <div class="box2">
                 <div class="box3">
                     <div class="box4">
@@ -57,10 +57,10 @@
             </div>
             <div class="box12">
                 <div class="box13">
-                    <ItineraryPreview class="q-ma-lg" id="preview" style="margin:0px;"></ItineraryPreview>
+                    <ItineraryPreview class="q-ma-lg"  id="preview" style="margin:0px;"></ItineraryPreview>
                 </div>
             </div>
-        </q-card>
+        </div>
         <div>
         <q-card class="box20 lt-md row">
             <div class="box21">
@@ -167,46 +167,74 @@
             height: 32px;
         
         
+            /* Inside auto layout */
+            flex: none;
+            order: 1;
+            flex-grow: 0;
     }
 .type7{
-      
-        width: auto;
+        width: 112px;
+            height: 27px;
+        
             font-family: 'Poppins';
             font-style: normal;
             font-weight: 500;
             font-size: 18px;
-            
-            
+            line-height: 27px;
+            /* identical to box height */
+            text-align: center;
         
             color: #FFFFFF;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
     }
 .type6{
+        width: 112px;
+            height: 27px;
         
-      width: 0 auto;
             font-family: 'Poppins';
             font-style: normal;
             font-weight: 500;
             font-size: 18px;
-           
-           
+            line-height: 27px;
+            /* identical to box height */
+            text-align: center;
         
             color: #FFFFFF;
         
         
-           
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
     }
 .box13{
         display: flex;
             flex-direction: column;
             align-items: flex-start;
-            
+            padding: 0px;
             gap: 10px;
+        
+            position: absolute;
+            width: 489px;
+            height: fit-content;
+            left: 0px;
+            top: 0px;
         
             background: #E5E3E6;
     }
 .box12{
-        
+        position: absolute;
+            width: 489px;
+            height: 600px;
+            left: 0px;
+            top: 0px;
             overflow-y: scroll;
+        
             background: #FFFFFF;
             border-radius: 16px 0px 0px 16px;
     }
@@ -218,25 +246,40 @@
             padding: 10px 40px;
             gap: 21px;
         
-          
+            width: 245px;
+            height: 52px;
+        
+            /* Travel Rover/Primary Color */
             background: #003FA3;
             border-radius: 6px;
         
-           
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
     }
 .box10{
         display: flex;
             flex-direction: column;
             align-items: flex-start;
-           
+            padding: 0px;
             gap: 20px;
-           
+        
+            width: 245px;
+            height: 124px;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 1;
+            flex-grow: 0;
     }
     .type5{
-        width: 180px;
+        width: 345px;
     }
 .type4{
-       
+        width: 363px;
+            height: 42px;
         
             font-family: 'Poppins';
             flex-direction: row;
@@ -244,125 +287,219 @@
             font-weight: 700;
             font-size: 28px;
             line-height: 42px;
-      
+            /* identical to box height */
+            text-align: justify;
+        
             color: #2F2F42;
         
         
-            
+            /* Inside auto layout */
+            flex: none;
+            order: 1;
+            flex-grow: 0;
     }
 .type3{
-       
+        width: 78px;
+            height: 21px;
         
             font-family: 'Poppins';
             font-style: normal;
             font-weight: 400;
             font-size: 14px;
-           
-            
-           
+            line-height: 21px;
+            /* identical to box height */
+            text-align: justify;
         
             color: #484848;
-    
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
     }
 .box9{
         display: flex;
             flex-direction: column;
             align-items: flex-start;
+            padding: 0px;
+        
+            width: 163px;
+            height: 63px;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
     }
 .box8{
         display: flex;
             flex-direction: column;
             align-items: center;
-        
+            padding: 0px;
             gap: 16px;
         
-           
+            width: 163px;
+            height: 63px;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
     }
 .box7{
         display: flex;
             flex-direction: column;
             align-items: flex-start;
+            padding: 0px;
             gap: 20px;
         
+            width: 245px;
+            height: 207px;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 1;
+            flex-grow: 0;
     }
 .type2{
-        max-width: 215px;
-            
+        width: 215px;
+            height: 42px;
         
             font-family: 'Poppins';
             font-style: normal;
             font-weight: 400;
             font-size: 14px;
-            
+            line-height: 21px;
         
             color: #555555;
         
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 1;
+            flex-grow: 1;
     }
 .box7{
         width: 24px;
             height: 24px;
         
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
     }
 .box6{
         display: flex;
             flex-direction: row;
             align-items: flex-start;
-           
+            padding: 0px;
             gap: 6px;
         
-            
+            width: 245px;
+            height: 42px;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            align-self: stretch;
+            flex-grow: 0;
     }
 .box5{
         display: flex;
             flex-direction: column;
             align-items: flex-start;
-           
+            padding: 0px;
             gap: 10px;
         
-            
+            width: 245px;
+            height: 198px;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 1;
+            align-self: stretch;
+            flex-grow: 0;
     }
 .type1{
-       
+        width: 345px;
+            height: 30px;
         
             font-family: 'Poppins';
             font-style: normal;
             font-weight: 600;
             font-size: 18.5px;
             line-height: 30px;
-           
+            /* identical to box height */
         
             color: #202020;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
     }
 .box4{
         display: flex;
             flex-direction: column;
             align-items: flex-start;
+            padding: 0px;
             gap: 31px;
         
-           
+            width: 245px;
+            height: 30px;
+        
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
     }
 .box3{
         display: flex;
             flex-direction: column;
             align-items: flex-start;
-           
+            padding: 0px;
             gap: 20px;
         
-            
+            width: 245px;
+            height: 248px;
         
+        
+            /* Inside auto layout */
+            flex: none;
+            order: 0;
+            flex-grow: 0;
     }
 .box2{
         display: flex;
             flex-direction: column;
             justify-content: center;
-            height: 700px;
-            gap: 20px;
-            
+            align-items: flex-start;
+            padding: 0px;
+            gap: 65px;
+        
+            position: absolute;
+            width: 245px;
+            height: 420px;
+            left: 543px;
+            top: 0px;
+            bottom: 150px;
     }
 .box1{
-        display: flex;
-        width: fit-content;
+    position: relative;
+        width: 842px;
+        height: 600px;
+    
         background: #FFFFFF;
+        border-radius: 16px;
         box-shadow: 0px 0px 14px rgba(0, 0, 0, 0.25);
 }
 </style>
